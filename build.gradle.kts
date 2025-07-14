79f4add76f56cebd202148c3cdd3ac5e83b5d5c6
Diógenes Falcão <diogenes.araujo@gmail.com>
2025-07-14 02:32:30 -0300

    [tests] Ensure we're running test cases

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -19,8 +20,22 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+
+    // Import the JUnit 5 BOM to manage and align all JUnit dependency versions.
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
+    // Use the kotlin-test-junit5 adapter for JUnit 5 to match `useJUnitPlatform()`.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    testLogging {
+        showStandardStreams = true
+        exceptionFormat = TestExceptionFormat.FULL
+        events("passed", "skipped", "failed")
+    }
 }
