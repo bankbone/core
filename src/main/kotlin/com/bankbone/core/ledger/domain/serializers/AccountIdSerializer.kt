0aae5b846e91a0855cd8551b4520693e49de7b24
Diógenes Falcão <diogenes.araujo@gmail.com>
2025-08-11 01:07:44 -0300

    chore: remove unused serializer files

--- src/main/kotlin/com/bankbone/core/ledger/domain/serializers/AccountIdSerializer.kt
+++
@@ -1,9 +0,0 @@
-package com.bankbone.core.ledger.domain.serializers
-
-import com.bankbone.core.ledger.domain.Account
-import com.bankbone.core.sharedkernel.infrastructure.serialization.UuidBasedIdSerializer
-
-object AccountIdSerializer : UuidBasedIdSerializer<Account.Id>(
-    serialName = "com.bankbone.core.ledger.domain.Account.Id",
-    fromUuid = { uuid -> Account.Id(uuid) }
-)
--- src/main/kotlin/com/bankbone/core/ledger/domain/serializers/LedgerTransactionIdSerializer.kt
+++
@@ -1,9 +0,0 @@
-package com.bankbone.core.ledger.domain.serializers
-
-import com.bankbone.core.ledger.domain.LedgerTransaction
-import com.bankbone.core.sharedkernel.infrastructure.serialization.UuidBasedIdSerializer
-
-object LedgerTransactionIdSerializer : UuidBasedIdSerializer<LedgerTransaction.Id>(
-    serialName = "com.bankbone.core.ledger.domain.LedgerTransaction.Id",
-    fromUuid = { uuid -> LedgerTransaction.Id(uuid) }
-)
